ba543b1415bda1369ff40c1aa4ca77df2984de45
fernandomarques <fernando.marquesfilho@gmail.com>
2017-04-25 11:38:54 -0300

    Exercicio modificado

--- tp2/alunos/sorteio.py
+++ tp2/alunos/sorteio.py
@@ -1,6 +1,19 @@
 import random
 
-alunos = ["Allex", "Igor", "Johelen","Juliana", "Leonardo",
-	"Lucas Aleixo", "Marcos", "Paulo"]
-	
-print(random.choice(alunos))
+
+def main():
+	alunos = ["Allex", "Igor", "Johelen","Juliana", "Leonardo",
+		"Lucas Aleixo", "Marcos", "Paulo"]
+	_alunos = alunos[:]
+	while(True):
+		if _alunos == []:
+			_alunos = alunos[:]
+		aluno = random.choice(_alunos)
+		_alunos.remove(aluno)
+		print(aluno)
+		a = input("")
+		if a == 'q':
+			return
+
+if __name__ == "__main__":
+	main()
