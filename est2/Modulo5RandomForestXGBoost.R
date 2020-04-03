24fd32f150e1cbaf6c4252b5488f262002526748
Fernando Marques <fernando.marquesfilho@gmail.com>
2020-04-03 17:30:48 -0300

    Quebrado as unidade

--- est2/Modulo5RandomForestXGBoost.R
+++ est2/Modulo5RandomForestXGBoost.R
@@ -1,76 +1,8 @@
 
-library(dplyr)
-loan_data <- read.csv('loan_data.csv')
-loan_data <- select(loan_data, -X, -status)
+sp500_px <- read.csv("sp500_data.csv")
 
-seed <- 400820
-set.seed(seed = seed)
-predictors <- data.matrix(loan_data[,-which(names(loan_data) %in% 'outcome')])
-label <- as.numeric(loan_data$outcome)-1
-test_idx <- sample(nrow(loan_data), 10000)
-library(xgboost)
-xgb_default <- xgboost(data=predictors[-test_idx,], label=label[-test_idx], 
-                       objective = "binary:logistic", nrounds=250, verbose=0)
-pred_default <- predict(xgb_default, predictors[test_idx,])
-error_default <- abs(label[test_idx] - pred_default) > 0.5
-xgb_default$evaluation_log[250,]
-mean(error_default)
+df <- sp500_px[row.names(sp500_px)>='2011-01-01', c('XOM', 'CVX')]
+set.seed(52)
+km <- kmeans(df, nstart = 10,centers=4 )
 
-
-xgb_penalty <- xgboost(data=predictors[-test_idx,], 
-                       label=label[-test_idx], 
-                       params=list(eta=.1, subsample=.63, lambda=1000),
-                       objective = "binary:logistic", nrounds=250, verbose=0)
-pred_penalty <- predict(xgb_penalty, predictors[test_idx,])
-error_penalty <- abs(label[test_idx] - pred_penalty) > 0.5
-xgb_penalty$evaluation_log[250,]
-mean(error_penalty)
-
-
-error_default <- rep(0, 250)
-error_penalty <- rep(0, 250)
-for(i in 1:250)
-{
-  pred_default <- predict(xgb_default, predictors[test_idx,], ntreelimit = i)
-  error_default[i] <- mean(abs(label[test_idx] - pred_default) > 0.5)
-  pred_penalty <- predict(xgb_penalty, predictors[test_idx,], ntreelimit = i)
-  error_penalty[i] <- mean(abs(label[test_idx] - pred_penalty) > 0.5)
-}
-
-errors <- rbind(xgb_default$evaluation_log,
-                xgb_penalty$evaluation_log,
-                data.frame(iter=1:250, train_error=error_default),
-                data.frame(iter=1:250, train_error=error_penalty))
-errors$type <- rep(c('default train', 'penalty train', 
-                     'default test', 'penalty test'), rep(250, 4))
-library(ggplot2)
-ggplot(errors, aes(x=iter, y=train_error, group=type)) +
-  geom_line(aes(linetype=type, color=type), size=1) +
-  scale_linetype_manual(values=c('solid', 'dashed', 'dotted', 'longdash')) +
-  theme_bw() +
-  theme(legend.key.width = unit(1.5,"cm")) +
-  labs(x="Iterations", y="Error") +
-  guides(colour = guide_legend(override.aes = list(size=1)))
-
-
-N <- nrow(loan_data)
-fold_number <- sample(1:5, N, replace = TRUE)
-params <- data.frame(eta = rep(c(.1, .5, .9), 3),
-                     max_depth = rep(c(3, 6, 12), rep(3,3)))
-rf_list <- vector('list', 9)
-error <- matrix(0, nrow=9, ncol=5)
-for(i in 1:nrow(params)){
-  for(k in 1:5){
-    cat('Fold', k, 'for model', i, '\n')
-    fold_idx <- (1:N)[fold_number == k]
-    xgb <- xgboost(data=predictors[-fold_idx,], label=label[-fold_idx], 
-                   params = list(eta = params[i, 'eta'], 
-                                 max_depth = params[i, 'max_depth']),
-                   objective = "binary:logistic", nrounds=100, verbose=0)
-    pred <- predict(xgb, predictors[fold_idx,])
-    error[i, k] <- mean(abs(label[fold_idx] - pred) >= 0.5)
-  }
-}
-
-avg_error <- 100 * round(rowMeans(error), 4)
-cbind(params, avg_error)
+km$centers
